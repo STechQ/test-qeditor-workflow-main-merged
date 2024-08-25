14abc2bb3ae3a853b725a7558c48c8b8ebb20c95
STechQ <stech.quick@yandex.com>
2024-08-25 15:52:50 +0000

    deploy for test

--- flow-editor/dist/common/ui/components/comps/Mapping.d.ts
+++ flow-editor/dist/common/ui/components/comps/Mapping.d.ts
@@ -5,6 +5,10 @@ interface IMappingProps {
     mapping: IStoreMapping;
     mappingSchema: IStoreMapping;
     outerStore: INestedStore;
+    constantsOptions?: Array<{
+        name: string;
+        value: string;
+    }>;
     readOnly?: boolean;
     onChangeInParamMapping: (inParamMapping: IStoreInParameterMapping[]) => void;
     onChangeOutParamMapping: (outParamMapping: IStoreOutParameterMapping[]) => void;
